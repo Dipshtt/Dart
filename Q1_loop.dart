4afa0f5db629b6c386479d53874a004342ef63b2
Dipshtt <108657125+Dipshtt@users.noreply.github.com>
2022-07-04 13:38:37 +0545

    Add files via upload

---
+++ Q1_loop.dart
@@ -0,0 +1,18 @@
+void main() {
+  print("\nFor Loop\n");
+  for (int i = 0; i < 5; i++) {
+    print("HELLO SUNWAY");
+  }
+  print("\nWHile Loop\n");
+  int i=0;
+  while (i < 5) {
+    print("HELLO SUNWAY");
+    i++;
+  }
+  print("\nDo While Loop\n");
+  int loop = 0;
+  do {
+    print("HELLO SUNWAY");
+    loop++;
+  } while (loop < 3);
+}
